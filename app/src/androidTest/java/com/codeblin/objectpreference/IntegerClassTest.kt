6a87fa38e5d734a11dcd8bb2e98e2045f7c995e9
sstiliatis <sstiliatis@gamingsynergies.com>
2020-01-30 11:41:25 +0200

    # Test summary

--- app/src/androidTest/java/com/codeblin/objectpreference/IntegerClassTest.kt
+++ app/src/androidTest/java/com/codeblin/objectpreference/IntegerClassTest.kt
@@ -11,7 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class PrimitiveClassTest {
+class IntegerClassTest {
     companion object{
         const val ASSERTION_VALUE = 100
     }
